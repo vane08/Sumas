ee2b6d22261ada62d241139bbc2a6f0454fed918
vane08 <82453121+vane08@users.noreply.github.com>
2021-05-01 00:40:29 -0500

    Add files via upload

---
+++ SumaSuperior4VanessaCruzHuitron3202.m
@@ -0,0 +1,23 @@
+%Titulo:Suma Superior (4)
+%Nombre:Vanessa Cruz Huitrón 
+%Grupo:3202
+%Fecha: Viernes 30 de Abril del 2021
+
+%f(x)=x^(2 )+2
+
+%Limpiar variables
+clear
+funcion [int]= área (diversion,a,b,n)
+f= en linea (divertido);
+%paso de la funcion 
+%Base de los rectangulos
+h= (ba) /n;
+%Numero de intervalos
+x= a:h:b;
+int=0;
+%Inicia un ciclo
+para i=1:n
+%Calcular punto medio
+xn=(x(i)+x(i+1))/2;
+%Valor del area de uno de los rectangulos
+int=int+f(xn)*h;
