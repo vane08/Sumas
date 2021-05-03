128f0b753072cdc9f6810ce23229172adb3d9bf5
vane08 <82453121+vane08@users.noreply.github.com>
2021-05-03 13:34:57 -0500

    Add files via upload

---
+++ SumasInteriores5VanessaCruzHuitron3202.m
@@ -0,0 +1,23 @@
+%Titulo:Suma Inferior (5)
+%Nombre:Vanessa Cruz Huitrón 
+%Grupo:3202
+%Fecha: Viernes 30 de Abril del 2021
+%f(x)=3 1/3  x^2;[-3,3]
+
+%Limpiar variables
+clear
+function [int]=area(fun,a,b,n)
+f= inline (fun);
+%paso de la funcion 
+%Base de los rectangulos
+h= (b-a) /n;
+%Numero de intervalos
+x= a:h:b;
+int=0;
+%Inicia un ciclo
+for i=1:n
+%Calcular punto medio
+xn=(x(i)+x(i+1))/2;
+%Valor del area de uno de los rectangulos
+int=int+f(xn)*h;
+end
